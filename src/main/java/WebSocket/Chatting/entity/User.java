95af10e6222681258f2e8df02a54d2fa9a1388dd
(Jun) <(ddoly0106@gmail.com)>
2023-05-29 15:09:34 +0900

    엔티티

---
+++ src/main/java/WebSocket/Chatting/entity/User.java
@@ -0,0 +1,30 @@
+package WebSocket.Chatting.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickName;
+
+    private String email;
+
+    private String passWord;
+
+    private String provider;
+
+}
